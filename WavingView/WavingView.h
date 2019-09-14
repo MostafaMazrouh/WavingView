cc57e507ba1e57dc59e206150c832e87faa46dc9
Mostafa <mostafa.mazrouh@gmail.com>
2019-09-14 18:29:34 +0200

    Initial project setup

---
+++ WavingView/WavingView.h
@@ -0,0 +1,19 @@
+//
+//  WavingView.h
+//  WavingView
+//
+//  Created by Mostafa on 9/14/19.
+//  Copyright © 2019 Mostafa Mazrouh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WavingView.
+FOUNDATION_EXPORT double WavingViewVersionNumber;
+
+//! Project version string for WavingView.
+FOUNDATION_EXPORT const unsigned char WavingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WavingView/PublicHeader.h>
+
+
